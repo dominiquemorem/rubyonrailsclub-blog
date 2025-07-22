6d385d13cef85ef99b7f86354fcc77762ae54504
Dominique Morem <domi.bcastro@gmail.com>
2025-07-22 13:19:57 -0300

    Realizando separacao entre  a área pública e administrativa do site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles
+  get "welcome/index"
+  namespace :administrate do
+    resources :articles
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  root "welcome#index"
+
+  get "/inicio", to: "welcome#index"
 end
